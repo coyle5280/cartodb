c3fb90f5fcdac3c4f21e64f7b015a96276839eba
Guido Fioravantti <guido.fioravantti@gmail.com>
2017-02-06 17:30:52 +0100

    bring alias column back

--- db/migrate/20170206160007_add_alias_to_user_tables.rb
+++ db/migrate/20170206160007_add_alias_to_user_tables.rb
@@ -1,12 +1,14 @@
 Sequel.migration do
   up do
     alter_table :user_tables do
+      add_column :alias, :text
       add_column :schema_alias, 'json'
     end
   end
 
   down do
     alter_table :user_tables do
+      drop_column :alias
       drop_column :schema_alias
     end
   end
